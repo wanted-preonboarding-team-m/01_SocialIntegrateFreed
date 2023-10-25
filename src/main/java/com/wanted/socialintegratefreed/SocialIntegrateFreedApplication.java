b1a438b9e26971f8e26a12096bf40c56cdf8dec9
kawksung <alstjd5479@naver.com>
2023-10-25 10:07:41 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/wanted/socialintegratefreed/SocialIntegrateFreedApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.socialintegratefreed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialIntegrateFreedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialIntegrateFreedApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/wanted/socialintegratefreed/SocialIntegrateFreedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.socialintegratefreed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialIntegrateFreedApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
