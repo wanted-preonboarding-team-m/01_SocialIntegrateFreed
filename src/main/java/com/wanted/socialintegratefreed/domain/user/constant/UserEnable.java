9df059545d5b7b296071434e62c298beebc67f8a
jhva <junghun8158@naver.com>
2023-10-25 14:41:00 +0900

    :truck: chore: UserEnable 클래스 주석 추가

--- src/main/java/com/wanted/socialintegratefreed/domain/user/constant/UserEnable.java
+++ src/main/java/com/wanted/socialintegratefreed/domain/user/constant/UserEnable.java
@@ -5,10 +5,16 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+/**
+ * UserEnable: 로그인 여부
+ */
 public enum UserEnable {
+    /**
+     * @USER_ENABLED : 가입 여부 활성화
+     * @USER_DISABLED : 가입 여부 비활성화
+     */
     USER_ENABLED("활성화"),
     USER_DISABLED("비활성화");
     private String enable;
-
 
 }
