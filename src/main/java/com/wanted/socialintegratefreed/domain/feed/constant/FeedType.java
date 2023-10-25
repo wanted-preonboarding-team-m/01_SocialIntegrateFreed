36e01bf87d679a936be4c3f4b1f1a393eb380012
Ingyu Song <ingyu3879@gmail.com>
2023-10-25 18:58:54 +0900

    feat: 게시물 관련 Entity, Dao 생성 및 테이블 간 연관 관계 설정

---
+++ src/main/java/com/wanted/socialintegratefreed/domain/feed/constant/FeedType.java
@@ -0,0 +1,9 @@
+package com.wanted.socialintegratefreed.domain.feed.constant;
+
+/**
+ * 게시물 타입  Enum
+ */
+public enum FeedType {
+  FACEBOOK, TWITTER, INSTAGRAM, THREADS
+
+}
---
+++ src/main/java/com/wanted/socialintegratefreed/domain/tagmatching/entity/TagMatching.java
@@ -0,0 +1,53 @@
+package com.wanted.socialintegratefreed.domain.tagmatching.entity;
+
+import com.wanted.socialintegratefreed.domain.feed.entity.Feed;
+import com.wanted.socialintegratefreed.domain.hashtag.entity.Hashtag;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+
+/**
+ * `hashtag`, `feed` 엔티티와의 다대다 관계를 일대다 - 다대일 관계로 매핑하기 위한 중간 엔티티
+ */
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "tag_matching")
+public class TagMatching {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "tag_matching_id", nullable = false)
+  private Long tagMatchingId;
+
+  // 부모 엔티티인 'Feed'와의 관계 설정
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "feed_id")
+  @Cascade({CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+  private Feed feed;
+
+  // 부모 엔티티인 'Hashtag'와의 관계 설정
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "hashtag_id")
+  @Cascade({CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+  private Hashtag hashtag;
+
+  @Builder
+  public TagMatching(Feed feed, Hashtag hashtag) {
+    this.feed = feed;
+    this.hashtag = hashtag;
+  }
+}
