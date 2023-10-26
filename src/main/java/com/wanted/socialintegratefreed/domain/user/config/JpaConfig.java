4c90d64927173b0a22119edd2d7aa958c25f28c0
jhva <junghun8158@naver.com>
2023-10-26 12:14:57 +0900

    :sparkles: feat: jpaConfig 추가

---
+++ src/main/java/com/wanted/socialintegratefreed/domain/user/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.wanted.socialintegratefreed.domain.user.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+}
