92fc71a900006b4d3b0c58fafabb9e6c0be46236
jhva <junghun8158@naver.com>
2023-10-26 14:16:50 +0900

    :sparkles: feat: errorcode enum 추가

--- src/main/java/com/wanted/socialintegratefreed/global/error/ErrorCode.java
+++ src/main/java/com/wanted/socialintegratefreed/global/error/ErrorCode.java
@@ -8,15 +8,24 @@ import org.springframework.http.HttpStatus;
  */
 @Getter
 public enum ErrorCode {
-  ;
 
-  //오류 메시지
-  private final String message;
-  //오류 상태코드
-  private final HttpStatus httpStatus;
+    DUPLICATE_EMAIL("중복 된 이메일 입니다.", HttpStatus.BAD_REQUEST),
+    PASSWORD_NOT_MATCH("비밀번호가 맞지 않습니다.", HttpStatus.BAD_REQUEST),
+    ACCESS_DENIED_EXCEPTION("접근이 거부되었습니다.", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND("해당 유저는 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
+    EMAIL_NOT_EXIST("이메일이 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
+    CODE_MISMATCH("일치하지 않는 인증번호 입니다.", HttpStatus.BAD_REQUEST),
+    EXIST_NOT_SESSION("세션이 만료되었습니다. 다시 인증코드를 발급해주세요.", HttpStatus.BAD_REQUEST);
 
-  ErrorCode(String message, HttpStatus httpStatus) {
-    this.message = message;
-    this.httpStatus = httpStatus;
-  }
+
+    //오류 메시지
+    private final String message;
+    //오류 상태코드
+    private final HttpStatus httpStatus;
+
+    ErrorCode(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
 }
