678f660bb515608315b6f8d8156c6bedd92c7433
jhva <junghun8158@naver.com>
2023-10-26 12:20:23 +0900

    :sparkles: feat: UserRequestAuthCodeDto 추가

---
+++ src/main/java/com/wanted/socialintegratefreed/domain/user/dto/request/UserRequestAuthCodeDto.java
@@ -0,0 +1,24 @@
+package com.wanted.socialintegratefreed.domain.user.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+
+/**
+ * 인증코드 체킹을 위한 dto
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class UserRequestAuthCodeDto {
+
+    // 회원가입 할때 받았던 코드
+    private int code;
+
+    // email이 세션에 있는지 확인하기위한
+    private String verifierEmail;
+
+
+}
