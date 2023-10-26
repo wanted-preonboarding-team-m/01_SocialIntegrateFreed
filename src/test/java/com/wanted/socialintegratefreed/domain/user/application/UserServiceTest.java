93434b63a91db1e770ac301bed9279b797d55bc1
jhva <junghun8158@naver.com>
2023-10-26 20:21:20 +0900

    :white_check_mark: test: test createUser in userServiceTest

--- src/test/java/com/wanted/socialintegratefreed/domain/user/application/UserServiceTest.java
+++ src/test/java/com/wanted/socialintegratefreed/domain/user/application/UserServiceTest.java
@@ -1,12 +1,19 @@
 package com.wanted.socialintegratefreed.domain.user.application;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
+import com.wanted.socialintegratefreed.domain.user.constant.UserEnable;
 import com.wanted.socialintegratefreed.domain.user.dao.UserRepository;
 import com.wanted.socialintegratefreed.domain.user.dto.request.UserRequestDto;
 import com.wanted.socialintegratefreed.domain.user.entity.User;
+import com.wanted.socialintegratefreed.global.error.BusinessException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +22,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -28,12 +41,20 @@ public class UserServiceTest {
     private User user;
     private UserRequestDto userRequestDto;
 
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+
     @BeforeEach
     void setUp() {
         user = User.builder()
                 .email("junghun8158@naver.com")
                 .password("1234")
                 .build();
+        userRequestDto = UserRequestDto.builder()
+                .email("test@example.com")
+                .password("asdofijsdaoif234").build();
+        MockitoAnnotations.openMocks(this);
 
     }
 
@@ -45,6 +66,13 @@ public class UserServiceTest {
         @DisplayName("사용자 등록에 성공한다.")
         void 사용자_등록에_성공한다() {
 
+            HttpServletRequest httpServletRequest = new MockHttpServletRequest();
+
+            //when
+            int authCode = userService.signUp(userRequestDto, httpServletRequest);
+
+            // then
+            assertEquals(6, String.valueOf(authCode).length()); // 6자리 랜덤 인증 코드 확인
         }
     }
 }
