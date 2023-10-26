aa615c3b57617c7f62bd66fc0a9e4dbac2e0d7f8
jhva <junghun8158@naver.com>
2023-10-26 12:15:48 +0900

    :sparkles: feat: findByEmail 추가

--- src/main/java/com/wanted/socialintegratefreed/domain/user/dao/UserRepository.java
+++ src/main/java/com/wanted/socialintegratefreed/domain/user/dao/UserRepository.java
@@ -3,8 +3,10 @@ package com.wanted.socialintegratefreed.domain.user.dao;
 
 import com.wanted.socialintegratefreed.domain.user.entity.User;
 import jakarta.persistence.Id;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Id> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
 }
