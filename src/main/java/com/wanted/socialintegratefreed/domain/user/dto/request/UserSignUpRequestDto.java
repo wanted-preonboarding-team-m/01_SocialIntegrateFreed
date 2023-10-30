6c4e2aab82993fd5f0acc04b77382e47d5360471
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-10-30 10:16:55 +0900

    fix: userRequestDto 2개에 @NoArgsConstructor

--- src/main/java/com/wanted/socialintegratefreed/domain/user/dto/request/UserSignUpRequestDto.java
+++ src/main/java/com/wanted/socialintegratefreed/domain/user/dto/request/UserSignUpRequestDto.java
@@ -9,12 +9,14 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 회원가입 할때 사용하는 dto
  */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 @Builder
 public class UserSignUpRequestDto {
 
